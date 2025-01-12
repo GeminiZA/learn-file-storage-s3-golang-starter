1f73308542835865b60f721e6ac109eb0f1a6b37
Tristan Theron <tristantheron@gmail.com>
2025-01-12 13:44:02 +0200

    ch1

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +34,78 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse form", err)
+		return
+	}
+
+	file, fileHeaders, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get form file", err)
+		return
+	}
+
+	fileMediaType := fileHeaders.Header.Get("Content-Type")
+
+	mt, _, err := mime.ParseMediaType(fileMediaType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couln't read Media Type", err)
+		return
+	}
+	if mt != "image/jpeg" && mt != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", err)
+		return
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read file data", err)
+		return
+	}
+
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video metadata", err)
+		return
+	}
+
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized", err)
+		return
+	}
+
+	videoExtension := strings.Split(fileMediaType, "/")[1]
+	videoFileName := fmt.Sprintf("%s.%s", videoID, videoExtension)
+	videoPath := filepath.Join(cfg.assetsRoot, videoFileName)
+
+	fmt.Println("Video path:", videoPath)
+
+	newThumbnailURL := fmt.Sprintf("http://localhost:%s/%s", cfg.port, videoPath)
+	metadata.ThumbnailURL = &newThumbnailURL
+
+	outFile, err := os.Create(videoPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create file", err)
+		return
+	}
+
+	_, err = io.Copy(outFile, bytes.NewReader(data))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write file", err)
+		return
+	}
+
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
